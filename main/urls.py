3587b12d47c92056d072352b10ccdb6cefcbcd42
Emad <empu.1001001@gmail.com>
2025-09-06 18:45:06 +0330

    Added Login and Logout Views

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, HomePageView, MyLoginView, MyLogoutView, DashboardView
 
 
 urlpatterns = [
-    path('tasks/', TaskListView.as_view()),
+    path('', HomePageView.as_view(), name='home'),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', MyLogoutView.as_view(), name='logout'),
+    path('dashboard/', DashboardView.as_view()),
+    path('tasks/', TaskListView.as_view(), name='tasks'),
 ]
