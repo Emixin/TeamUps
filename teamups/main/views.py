6cceaf65c075356537b55162081122a32738153c
Emad <empu.1001001@gmail.com>
2025-09-06 18:45:06 +0330

    Added Login and Logout Views

--- teamups/main/views.py
+++ teamups/main/views.py
@@ -1,13 +1,26 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView, DetailView
+from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages 
+from django.urls import reverse_lazy
 from .models import Task, Team
-from django.views.generic import FormView
 from .forms import MyLoginForm
 from .models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+
+
 
 
 User = get_user_model()
+
+
+class HomePageView(DetailView):
+    model = User
+    template_name = "main/home.html"
+
+    def get_object(self):
+        return self.request.user
 
 
 class MyLoginView(FormView):
@@ -27,9 +40,30 @@ class MyLoginView(FormView):
 
         user = authenticate(self.request, username=username, password=password)
 
+        if user:
+            login(self.request, user)
+            messages.success(self.request, f"Welcome {user.username}")
+            return redirect('home')
 
 
-class TaskListView(ListView):
+        messages.error(self.request, "Incorrect username or password")
+        return self.form_invalid(form)
+
+
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("home")
+
+
+
+class DashboardView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'main/dashboard.html'
+    context_object_name = 'user'
+
+
+
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'main/tasks_list.html'
     context_object_name = 'tasks'
