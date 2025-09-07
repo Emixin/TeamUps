4e72e0f9b28f213ebef77969ee57b8c70e0d55ab
Emad <empu.1001001@gmail.com>
2025-09-07 10:57:48 +0330

    Added SignUp

--- teamups/main/urls.py
+++ teamups/main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TaskListView, HomePageView, MyLoginView, MyLogoutView, DashboardView
+from .views import TaskListView, HomePageView, MyLoginView, MyLogoutView, DashboardView, MySignUpView
+
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('login/', MyLoginView.as_view(), name='login'),
+    path('signup/', MySignUpView.as_view(), name='signup'),
     path('logout/', MyLogoutView.as_view(), name='logout'),
     path('dashboard/', DashboardView.as_view()),
     path('tasks/', TaskListView.as_view(), name='tasks'),
