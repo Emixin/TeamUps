b14bd0dd2715bb4a7033b1de6ec9a40946b7edd1
Emad <empu.1001001@gmail.com>
2025-09-08 12:13:49 +0330

    Fixed SignUp Process

--- teamups/main/forms.py
+++ teamups/main/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class MyLoginForm(forms.Form):
     email_or_username = forms.CharField(label="Email or Username")
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class MySignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
--- teamups/main/urls.py
+++ teamups/main/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', MyLoginView.as_view(), name='login'),
     path('signup/', MySignUpView.as_view(), name='signup'),
     path('logout/', MyLogoutView.as_view(), name='logout'),
-    path('dashboard/', DashboardView.as_view()),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('tasks/', TaskListView.as_view(), name='tasks'),
 ]
