0629564c60f66eec218033322b168636ffc2c4b5
Emad <empu.1001001@gmail.com>
2025-09-11 20:04:09 +0330

    Adding leader to team members

--- teamups/main/forms.py
+++ teamups/main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import Team, Task
 
 
 User = get_user_model()
@@ -14,3 +15,26 @@ class MySignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'max_members', 'leader']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["leader"].queryset = User.objects.filter(is_superuser=False)
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'deadline', 'team', 'task_type']
+
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.get("user")
+        super().__init__(*args, **kwargs)
+        self.fields["team"].queryset = Team.objects.filter(members=user)
+
--- teamups/main/urls.py
+++ teamups/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import TaskListView, HomePageView, MyLoginView, MyLogoutView, DashboardView, MySignUpView
-
+from .views import TaskListView, HomePageView, MyLoginView, MyLogoutView
+from .views import DashboardView, MySignUpView, TeamCreateView, TaskCreateView
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     path('signup/', MySignUpView.as_view(), name='signup'),
     path('logout/', MyLogoutView.as_view(), name='logout'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path("teams/create/", TeamCreateView.as_view(), name="create_team"),
+    path("tasks/create/", TaskCreateView.as_view(), name="create_task"),
     path('tasks/', TaskListView.as_view(), name='tasks'),
 ]
