38061f8466f0056368edb9eb90548b5035792b76
Emad <empu.1001001@gmail.com>
2025-09-19 11:10:23 +0330

    Added Invitation feature

--- teamups/main/urls.py
+++ teamups/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import TaskListView, TeamListView, HomePageView, MyLoginView, MyLogoutView
-from .views import DashboardView, MySignUpView, UsersRatingList, TeamDetailsView
+from .views import DashboardView, MySignUpView, UsersRatingList, TeamDetailsView, UserInvitationList
 
 
 urlpatterns = [
@@ -13,4 +13,5 @@ urlpatterns = [
     path('teams/', TeamListView.as_view(), name='teams'),
     path('users/', UsersRatingList.as_view(), name='ratings'),
     path('teams/details/<int:pk>/', TeamDetailsView.as_view(), name='team_details'),
+    path('user/invitations/<int:pk>/', UserInvitationList.as_view(), name='invitations'),
 ]
