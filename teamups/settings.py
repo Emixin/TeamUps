0b29eaf4eb1e6fea158350373ccfe332361a11af
Emad <empu.1001001@gmail.com>
2026-06-16 19:27:30 +0330

    Added STATIC_ROOT

--- teamups/settings.py
+++ teamups/settings.py
@@ -145,12 +145,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
